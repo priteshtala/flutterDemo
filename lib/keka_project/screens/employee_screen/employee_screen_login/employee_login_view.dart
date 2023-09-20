ccd5c3aef3ba5efd86b8c53b17326444475ca445
krish <krish.r@elaunchinfotech.in>
2023-09-20 15:04:34 +0530

    department_details_cubit.dart

--- lib/keka_project/screens/employee_screen/employee_screen_login/employee_login_view.dart
+++ lib/keka_project/screens/employee_screen/employee_screen_login/employee_login_view.dart
@@ -1,107 +1,3 @@
-// import 'package:finaldemo/keka_project/common/common_button.dart';
-// import 'package:finaldemo/keka_project/common/common_textformfield.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'employee_login_cubit.dart';
-// import 'employee_login_state.dart';
-//
-// class EmployeeLoginView extends StatefulWidget {
-//   static const String routeName = "/Employee_Login_View";
-//
-//   static Widget builder(BuildContext context) {
-//     return BlocProvider(
-//       create: (context) => EmployeeLoginCubit(
-//         EmployeeLoginState(
-//           emailController: TextEditingController(),
-//           passwordController: TextEditingController(),
-//         ),
-//       ),
-//       child: const EmployeeLoginView(),
-//     );
-//   }
-//
-//   const EmployeeLoginView({super.key});
-//
-//   @override
-//   State<EmployeeLoginView> createState() => _EmployeeLoginViewState();
-// }
-//
-// class _EmployeeLoginViewState extends State<EmployeeLoginView> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocBuilder<EmployeeLoginCubit, EmployeeLoginState>(
-//       builder: (context, state) {
-//         return Scaffold(
-//           appBar: AppBar(
-//             title: const Text(
-//               "Log-In",
-//             ),
-//           ),
-//           backgroundColor: Colors.white,
-//           body: SingleChildScrollView(
-//             child: Column(
-//               children: [
-//                 Center(
-//                   child: Image.network(
-//                     "https://img.freepik.com/free-photo/computer-security-with-login-password-padlock_107791-16191.jpg?w=996&t=st=1693455723~exp=1693456323~hmac=de3c3a646fb74dcbfc30389aa6b92728347549967de2a757592a0f07815fd607",
-//                     height: MediaQuery.of(context).size.height * 0.35,
-//                     fit: BoxFit.fill,
-//                     width: MediaQuery.of(context).size.width * 0.80,
-//                   ),
-//                 ),
-//                 Column(
-//                   children: [
-//                     CustomTextForm(
-//                       controller: state.emailController,
-//                       readOnly: false,
-//                       textCapitalization: TextCapitalization.none,
-//                       obscureText: false,
-//                       hintText: "Enter Your Email",
-//                       keyboardType: TextInputType.emailAddress,
-//                       prefixIcon: const Icon(Icons.email),
-//                     ),
-//                     CustomTextForm(
-//                       controller: state.passwordController,
-//                       readOnly: false,
-//                       textCapitalization: TextCapitalization.none,
-//                       obscureText: state.iconShowHide,
-//                       hintText: "Enter Your Password",
-//                       prefixIcon: const Icon(Icons.lock),
-//                       suffixIcon: IconButton(
-//                         onPressed: () => context.read<EmployeeLoginCubit>().visibility(),
-//                         icon:
-//                             state.iconShowHide ? const Icon(Icons.visibility_off_sharp) : const Icon(Icons.visibility),
-//                       ),
-//                     ),
-//                   ],
-//                 )
-//               ],
-//             ),
-//           ),
-//           bottomNavigationBar: SafeArea(
-//             minimum: const EdgeInsets.only(bottom: 8),
-//             child: Row(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 CustomButton(
-//                   onPressed: () {
-//                     context.read<EmployeeLoginCubit>().validation(context);
-//                   },
-//                   minWidth: 300,
-//                   child: const Text(
-//                     "Log-in",
-//                     style: TextStyle(color: Colors.white, fontSize: 20),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-
 import 'package:finaldemo/keka_project/common/common_button.dart';
 import 'package:finaldemo/keka_project/common/common_textformfield.dart';
 import 'package:flutter/material.dart';
@@ -137,7 +33,8 @@ class EmployeeLoginViewState extends State<EmployeeLoginView> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        foregroundColor: Colors.white, backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.green,
         title: const Text('Log-In'),
       ),
       body: BlocBuilder<EmployeeLoginCubit, EmployeeLoginState>(
@@ -207,21 +104,20 @@ class EmployeeLoginViewState extends State<EmployeeLoginView> {
                       child: Text("Login", style: TextStyle(color: Colors.white)),
                     ),
                     const SizedBox(height: 20),
-                    const Text(
-                      "------------ or ------------",
-                      style: TextStyle(color: Colors.black),
+                    Row(
+                      children: [
+                        Expanded(child: Divider(color: Colors.black, indent: 40, endIndent: 10)),
+                        Text("or"),
+                        Expanded(child: Divider(color: Colors.black, indent: 10, endIndent: 40)),
+                      ],
                     ),
                     const SizedBox(height: 20),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Image.asset("assets/images/google.png", width: 35),
-                        Image.asset(
-                            "assets/images/facebook.png.webp",
-                            width: 35),
-                        Image.asset(
-                            "assets/images/twitter.png",
-                            width: 35),
+                        Image.asset("assets/images/facebook.png.webp", width: 35),
+                        Image.asset("assets/images/twitter.png", width: 35),
                       ],
                     ),
                   ],
